384a6afdd5e21f765842ac31485f89b2e830ca34
hildasatih <hildasatih@gmail.com>
2023-10-23 14:06:47 -0700

    Tasks done

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 import sys
 
@@ -5,9 +6,9 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (False)
-
+    except (ValueError, TypeError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
+    else:
+        return True
 
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
-
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
-    for i in range(0, list_length):
+    n_list = []
+    for i in range(list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
         except TypeError:
@@ -16,7 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             div = 0
         finally:
-            new_list.append(div)
-    return (new_list)
-
-
+            n_list.append(div)
+    return n_list
